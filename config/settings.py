df783edcd2856347a657d327d0670c715f0ddecd
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-23 12:06:08 +0200

    Add initialization for decouple

--- config/settings.py
+++ config/settings.py
@@ -9,16 +9,20 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-from decouple import Csv, config
+from decouple import Config, RepositoryEnv, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# Decouple initialization.
+env_file = os.getenv("ENV_FILE", ".env")
+config = Config(RepositoryEnv(env_file))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
