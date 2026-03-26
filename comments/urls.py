06b8e778bec2b3634aa6030377ef7120748df8a8
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-26 11:45:54 +0200

    Add a basename

--- comments/urls.py
+++ comments/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 from comments.views import CommentViewSet
 
 router = SimpleRouter()
-router.register("comment", CommentViewSet)
+router.register(prefix="comment", viewset=CommentViewSet, basename="comment")
 
 urlpatterns = [
     path("v1/", include(router.urls)),
