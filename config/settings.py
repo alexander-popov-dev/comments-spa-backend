efdd284e3c396697b8d4c52037c50a87e63336a2
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-27 12:39:40 +0200

    Add pagination

--- config/settings.py
+++ config/settings.py
@@ -156,6 +156,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": config("PAGE_SIZE", default=25, cast=int),
 }
 
 SIMPLE_JWT = {
