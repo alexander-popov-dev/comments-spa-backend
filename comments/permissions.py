42119e236f89a9173b4eade3587eb1a504120e82
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-30 12:42:04 +0300

    Add docstrings

--- comments/permissions.py
+++ comments/permissions.py
@@ -2,5 +2,7 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
+    """Grants access only if the requesting user is the comment author."""
+
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
