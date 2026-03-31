43b45869eb607173c6d346d09f4bd164f03170f4
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-31 12:46:29 +0300

    Move to backend folder

--- backend/comments/migrations/0002_initial.py
+++ backend/comments/migrations/0002_initial.py
@@ -6,23 +6,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('comments', '0001_initial'),
+        ("comments", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='comment',
-            name='user',
+            model_name="comment",
+            name="user",
             field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.CASCADE,
-                to=settings.AUTH_USER_MODEL
+                blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
             ),
         ),
     ]
